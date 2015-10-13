21ba2d4de6488e136aeca2ab2b44c2b0b4555f86
akifc <Akif_ares@yahoo.ca>
2015-10-12 22:47:11 -0600

    Updated to support Instagram v7.9.0
    (Since 7.9.0 hasn't been pushed to every user yet, it still keeps the
    hook for 7.8.0)

--- src/com/akif/instagram/disabledt2l/Instagram.java
+++ src/com/akif/instagram/disabledt2l/Instagram.java
@@ -1,9 +1,7 @@
 package com.akif.instagram.disabledt2l; 
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
-import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import android.view.MotionEvent;
@@ -13,9 +11,8 @@ public class Instagram implements IXposedHookLoadPackage {
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
 		if (lpparam.packageName.equals("com.instagram.android")) {
-			try {
-				findAndHookMethod("com.instagram.feed.ui.a.r", lpparam.classLoader, "onDoubleTap", MotionEvent.class, XC_MethodReplacement.returnConstant(true));
-		    } catch (Throwable t) { XposedBridge.log(t); }  
-			}
-		};
+			findAndHookMethod("com.instagram.feed.ui.a.r", lpparam.classLoader, "onDoubleTap", MotionEvent.class, XC_MethodReplacement.returnConstant(true));
+			findAndHookMethod("com.instagram.feed.ui.a.y", lpparam.classLoader, "onDoubleTap", MotionEvent.class, XC_MethodReplacement.returnConstant(true)); 
+		}
+	};
 }
