658e5a8445ff4a245980af0ff2769dd39d75ed88
akifc <Akif_ares@yahoo.ca>
2015-10-15 13:41:43 -0600

    Update Instagram.java

--- src/com/akif/instagram/disabledt2l/Instagram.java
+++ src/com/akif/instagram/disabledt2l/Instagram.java
@@ -11,8 +11,8 @@ public class Instagram implements IXposedHookLoadPackage {
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
 		if (lpparam.packageName.equals("com.instagram.android")) {
-	try {		findAndHookMethod("com.instagram.feed.ui.a.r", lpparam.classLoader, "onDoubleTap", MotionEvent.class, XC_MethodReplacement.returnConstant(true));
-} catch (Throwable t) {			findAndHookMethod("com.instagram.feed.ui.a.y", lpparam.classLoader, "onDoubleTap", MotionEvent.class, XC_MethodReplacement.returnConstant(true)); 
+	try {		findAndHookMethod("com.instagram.feed.ui.a.kjk", lpparam.classLoader, "onDoubleTap", MotionEvent.class, XC_MethodReplacement.returnConstant(true));
+} catch (Throwable t) {			findAndHookMethod("com.instagram.feed.ui.a.yfd", lpparam.classLoader, "onDoubleTap", MotionEvent.class, XC_MethodReplacement.returnConstant(true)); 
 }
 		}
 	};
